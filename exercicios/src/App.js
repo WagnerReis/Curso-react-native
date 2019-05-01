baa550fec5c2d5869cf2624a88d94a236edd9ffb
Wagner Silva <wagnerreis1997@gmail.com>
2019-05-01 12:47:58 -0300

    Primeiro Componente

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet} from 'react-native'
+import Simples from './componentes/Simples'
 
 export default class App extends Component {
   render(){
       return (
 		  <View style={styles.container}>
-			  <Text style={styles.f20}>App!</Text>
+			  	<Simples texto='Flexível!!!!'/>
 		  </View>
       )
   }
@@ -17,7 +18,4 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
-	f20: {
-		fontSize: 40
-	}
 })

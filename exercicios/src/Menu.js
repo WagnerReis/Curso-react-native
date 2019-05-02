89fae359104f3dbb3984f5c17bc70760355db1d5
Wagner Silva <wagnerreis1997@gmail.com>
2019-05-02 12:34:07 -0300

    Componente de Classe & Estado

--- exercicios/src/Menu.js
+++ exercicios/src/Menu.js
@@ -3,19 +3,23 @@ import { createDrawerNavigator } from 'react-navigation'
 
 import Simples from './componentes/Simples'
 import ParImpar from './componentes/ParImpar'
-import { Inverter, MegaSena } from './componentes/Multi'
+import Inverter, { MegaSena } from './componentes/Multi'
+import Contador from './componentes/Contador'
 
 export default createDrawerNavigator ({
+    Contador: {
+        screen: () => <Contador numeroInicial={1000} />
+    },
     MegaSena: {
         screen: () => <MegaSena numeros={8} />,
-        navigationOptions: { title: 'Mega Sena'} //Altera o nome
+        navigationOptions: { title: 'Mega Sena' }
     },
     Inverter: {
-        screen: () => <Inverter texto='React Nativo' />
+        screen: () => <Inverter texto='React Nativo!' />
     },
     ParImpar: {
         screen: () => <ParImpar numero={30} />,
-        navigationOptions: { title: 'Par & Impar' }
+        navigationOptions: { title: 'Par & Ímpar' }
     },
     Simples: {
         screen: () => <Simples texto='Flexível!!!' />

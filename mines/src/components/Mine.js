6ad35200fd24e629ac55ce18e9b7c517b00edd4f
Wagner Silva <wagnerreis1997@gmail.com>
2019-06-15 12:21:58 -0300

    Projeto Campo Minado finalizado

--- mines/src/components/Mine.js
+++ mines/src/components/Mine.js
@@ -3,12 +3,12 @@ import { View, StyleSheet } from 'react-native'
 
 export default props => {
     return (
-        <View style={StyleSheet.container}>
-            <View style={StyleSheet.coreMine} />
-            <View style={StyleSheet.line} />
-            <View style={[styles.line, { transform: [{ rotate: '45deg' }]}]} />
-            <View style={[styles.line, { transform: [{ rotate: '90deg' }]}]} />
-            <View style={[styles.line, { transform: [{ rotate: '135deg' }]}]} />
+        <View style={styles.container}>
+            <View style={styles.coreMine} />
+            <View style={styles.line} />
+            <View style={[styles.line, { transform: [{ rotate: '45deg' }] }]} />
+            <View style={[styles.line, { transform: [{ rotate: '90deg' }] }]} />
+            <View style={[styles.line, { transform: [{ rotate: '135deg' }] }]} />
         </View>
     )
 }

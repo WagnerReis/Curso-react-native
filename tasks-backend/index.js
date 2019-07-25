c89cbda67a9597806c12c2e0a2dc7816cd039263
Wagner Silva <wagnerreis1997@gmail.com>
2019-07-25 09:15:19 -0300

    Resumo Sobre Express

--- tasks-backend/index.js
+++ tasks-backend/index.js
@@ -1,10 +1,31 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => {
-    res.status(200).send('Meu Backend!')
+app.use(bodyParser.json())
+
+function meuJson() {
+    return (req, res, next) => {
+        console.log('Antes de tudo: Meu middlewere...')
+        next()
+    }
+}
+
+app.get('/blabla/:valor', (req, res, next) => {
+    console.log('Func 0')
+    next()
+})
+
+app.get('/blabla/:valor', (req, res, next) => {
+    console.log('Func 1')
+    res.status(200).send('Meu Backend = ' + req.body.valor)
+    next()
+})
+
+app.get('/blabla/:valor', (req, res) => {
+    console.log('Func 2')
 })
 
 app.listen(3000, () => {
     console.log('Backend executando...')
-})
+}) 
